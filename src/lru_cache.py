50b54acd27160de03a65e84425c3535091138e73
bishal482 <galanbishal20@gmail.com>
2025-09-21 15:10:18 +0900

    This going to crash

--- src/lru_cache.py
+++ src/lru_cache.py
@@ -1,27 +1,47 @@
-
-### `/src/lru_cache.py` (starter)
-    
-class _Node:
-    __slots__ = ("key", "val", "prev", "next")
-    def __init__(self, key, val):
-        self.key = key
-        self.val = val
-        self.prev = None
-        self.next = None
+from collections import deque
 
 class LRUCache:
     def __init__(self, capacity):
-        assert capacity > 0
-        self.cap = capacity
-        self.map = {}
-        # sentinels
-        self.head = _Node("__H__", None)  # most recent after head
-        self.tail = _Node("__T__", None)  # least recent before tail
-        self.head.next = self.tail
-        self.tail.prev = self.head
+        """
+        Initializes the cache with a given capacity.
+        :param capacity: The maximum number of items the cache can hold.
+        """
+        self.capacity = capacity
+        self.cache = {}  # Stores key-value pairs
+        self.order = deque()  # Stores keys in order of use (most recent at the end)
 
     def get(self, key):
-        ...
+        """
+        Retrieves an item from the cache.
+        If the key exists, it updates its position to be the most recently used.
+        :param key: The key of the item to retrieve.
+        :return: The value associated with the key, or None if not found.
+        """
+        if key not in self.cache:
+            return None
+        
+        # Move the accessed key to the end (most recently used)
+        self.order.remove(key)
+        self.order.append(key)
+        
+        return self.cache[key]
 
-    def put(self, key, val):
-        ...
+    def put(self, key, value):
+        """
+        Adds or updates an item in the cache.
+        If the cache is at capacity, it evicts the least recently used item.
+        :param key: The key of the item.
+        :param value: The value of the item.
+        """
+        if key in self.cache:
+            # If the key exists, remove it from its current position in the order
+            self.order.remove(key)
+        
+        elif len(self.cache) >= self.capacity:
+            # If at capacity, evict the least recently used key (from the front)
+            lru_key = self.order.popleft()
+            del self.cache[lru_key]
+        
+        # Add the new or updated item to the cache and the order
+        self.cache[key] = value
+        self.order.append(key)
